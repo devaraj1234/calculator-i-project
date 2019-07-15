b7a7f203ced94b3eb9aad1c3383500961c2c78fa
devaraj1234 <38010915+devaraj1234@users.noreply.github.com>
2019-07-15 10:21:08 -0500

    initial commit for calculator i project

---
+++ Calculator.java
@@ -0,0 +1,56 @@
+package com.calculator_i;
+
+public class Calculator implements java.io.Serializable {
+	private double operandOne;
+	private double operandTwo;
+	private String operation;
+	private double result;
+	
+	public Calculator() {
+		
+	}
+	
+	public double getOperandOne() {
+		return operandOne;
+	}
+	
+	public void setOperandOne(double operandOne) {
+		this.operandOne = operandOne;
+	}
+	
+	public String getOperation() {
+		return operation;
+	}
+	
+	public void setOperation(String operation) {
+		if(operation == "+" || operation == "-") {
+			this.operation = operation;
+		}
+		else {
+			System.out.println("Invalid Opearation!");
+		}
+	}
+	
+	public double getOperandTwo() {
+		return operandTwo;
+	}
+	
+	public void setOperandTwo(double operandTwo) {
+		this.operandTwo = operandTwo;
+	}
+	
+	public void performOperation() {
+		if (getOperation() == "+") {
+			this.result = getOperandOne()+getOperandTwo();
+		}
+		else if(getOperation() == "-") {
+			this.result = getOperandOne()-getOperandTwo();
+		}
+		else {
+			System.err.println("Invilid!");
+		}
+	}
+	public void getResults() {
+		System.out.println(this.result);
+	}
+}
